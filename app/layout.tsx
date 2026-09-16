fc17d74257669ada11d834f9f488ae4a91129a2d
Jyatin kumar Singh <singhjyatin@gmail.com>
2026-09-16 07:19:31 +0530

    Set dark theme as default for reference homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
     return (
         <html lang="en" suppressHydrationWarning>
             <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-                <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
+                <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
                     <AppNavbar />
                     <CollaborativeCursors />
                     <FloatingShootToggleHost />
