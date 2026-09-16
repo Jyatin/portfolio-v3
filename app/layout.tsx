d22b8f13faeae4f62496fd7bae88eb7ac83faa3a
Jyatin kumar Singh <singhjyatin@gmail.com>
2026-09-16 08:10:55 +0530

    Mount isolated custom cursor once in root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import PageTransition from "./components/page-transition";
 import FloatingShootToggleHost from "./components/floating-shoot-toggle-host";
 import CollaborativeCursors from "./components/collaborative-cursors";
 import ProjectTiltRuntime from "./components/project-tilt-runtime";
+import CustomCursor from "./components/custom-cursor";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
@@ -35,6 +36,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
                     <CollaborativeCursors />
                     <FloatingShootToggleHost />
                     <ProjectTiltRuntime />
+                    <CustomCursor />
                     <SmoothScroll>
                         <PageTransition>{children}</PageTransition>
                     </SmoothScroll>
