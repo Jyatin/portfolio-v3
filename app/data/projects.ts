9ab786fe91856289b4da4134ba7f69101f98fc0d
Jyatin kumar Singh <singhjyatin@gmail.com>
2026-09-18 00:21:55 +0530

    fix: use laptop mockup for JalDrishti project image

--- app/data/projects.ts
+++ app/data/projects.ts
@@ -31,7 +31,7 @@ const projectCoreList: readonly ProjectCore[] = [
         live: "",
         featured: true,
         year: "2026",
-        image: "/images/projects/jaldrishti_laptop.svg",
+        image: "/images/projects/jaldrishti_mockup.svg",
     },
     {
         slug: "askpdf",
